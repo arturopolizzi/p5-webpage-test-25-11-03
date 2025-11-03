90f8c46ec2f24b2e9d84b5b62b094a5b7fa85aed
Arturo Polizzi <arturopolizzi1@gmail.com>
2025-11-03 16:42:00 +0000

    scroll tied to windowposY + dampening

--- sketch.js
+++ sketch.js
@@ -1,32 +1,40 @@
 let rotationAmount = 0
 let rotationS = 0.01
-let rotationA = 0
-let rotationSinMagnitude = 0.001
 
-let isScrolling = false
 let axis = [1, 0, 0];
-
 
 var canvas;
 function setup() {
   canvas = createCanvas(210, 80, WEBGL);
   canvas.parent('header-sketch-container');
+  prevScrollY = window.scrollY
 }
 
 function draw() {
-  // background(200);
   clear()
   
   rotationAmount += rotationS
   rotate(rotationAmount, axis);
-  // Draw a box.
-  box(200,40,10);
+
+  box(200,60,10);
   
+  if (prevScrollY != window.scrollY) {
+    rotationDiff = window.scrollY - prevScrollY
+    rotationDiffScaled = rotationDiff * 0.004
+    rotationAmount += rotationDiffScaled
+    rotationS = rotationDiffScaled
+  }
+  
+  if (rotationS != 0.01) {
+    rotationSDiff = 0.01 - rotationS
+    rotationSDiffScaled = rotationSDiff * 0.05
+    rotationS += rotationSDiffScaled
+  }
+  
+  prevScrollY = window.scrollY
 }
 
-//fook off m8
-
-function mouseWheel(event) {
-  isScrolling = true
-  rotationAmount += event.delta/50
-}
+// function mouseWheel(event) {
+//   isScrolling = true
+//   rotationAmount += event.delta/50
+// }
