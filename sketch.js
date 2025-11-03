4080484e779133e2ae8c20c33ee7ab43b3b8e82d
Arturo Polizzi <arturopolizzi1@gmail.com>
2025-11-03 13:48:40 +0000

    initial commit

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let rotationAmount = 0
+let rotationS = 0.01
+let rotationA = 0
+let rotationSinMagnitude = 0.001
+
+let isScrolling = false
+let axis = [1, 0, 0];
+
+
+var canvas;
+function setup() {
+  canvas = createCanvas(210, 80, WEBGL);
+  canvas.parent('header-sketch-container');
+}
+
+function draw() {
+  background(200);
+  
+  rotationAmount += rotationS
+  rotate(rotationAmount, axis);
+  // Draw a box.
+  box(200,40,10);
+  
+}
+
+
+
+function mouseWheel(event) {
+  isScrolling = true
+  rotationAmount += event.delta/50
+}
